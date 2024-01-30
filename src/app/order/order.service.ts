0efaa3fd7c71a393fba595dd974a71822c4b95da
Vinicius Alves Beló <alexguilhermeferreiraa@gmail.com>
2024-01-30 13:58:51 +0000

    Finishing order started

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from "@angular/core";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
 import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { Order } from "./order.model";
+import { Observable } from "rxjs";
+import { Http, Headers, RequestOptions } from "@angular/http";
+import { MEAT_API } from "app/app.api";
 
 @Injectable()
 export class OrderService{
 
-    constructor(private cartService: ShoppingCartService) { }
+    constructor(private cartService: ShoppingCartService,
+        private http: Http) { }
 
     itemsValue(): number {
         return this.cartService.total()
@@ -25,5 +30,18 @@ export class OrderService{
 
     remove(item: CartItem) {
         this.cartService.removeItem(item)
+    }
+
+    clear() {
+        this.cartService.clear()
+    }
+
+    checkOrder(order: Order): Observable<string> {
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.http.post(`${MEAT_API}/orders`,
+            JSON.stringify(order),
+            new RequestOptions({headers: headers}))
+            .map(response => response.json())
     }
 }
