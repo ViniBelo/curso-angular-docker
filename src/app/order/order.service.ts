c5f86def76bdab63e1f7c1234de53b7f05a55ae7
Vinicius Alves Beló <alexguilhermeferreiraa@gmail.com>
2024-01-31 16:24:04 +0000

    Login working

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -3,14 +3,16 @@ import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
 import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
 import { Order } from "./order.model";
 import { Observable } from "rxjs";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { MEAT_API } from "app/app.api";
+import { LoginService } from "app/security/login/login.service";
 
 @Injectable()
 export class OrderService{
 
     constructor(private cartService: ShoppingCartService,
-        private http: HttpClient) { }
+                private http: HttpClient,
+                private loginService: LoginService) { }
 
     itemsValue(): number {
         return this.cartService.total()
@@ -37,7 +39,11 @@ export class OrderService{
     }
 
     checkOrder(order: Order): Observable<string> {
-        return this.http.post<Order>(`${MEAT_API}/orders`, order)
+        let headers = new HttpHeaders()
+        if(this.loginService.isLoggedIn()) {
+            headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`)
+        }
+        return this.http.post<Order>(`${MEAT_API}/orders`, order, {headers: headers})
             .map(order => order.id)
     }
 }
